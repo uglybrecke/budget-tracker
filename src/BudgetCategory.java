f4f56aa9ff5745f4be55db3caa90873232cf59a2
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-04-22 11:28:55 -0700

    built out the BudgetCategory class

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,46 @@
 public class BudgetCategory {
+    private String name;
+    private double limit;
+    private double spent;
+
+    public BudgetCategory(String name, double limit, double spent) {
+        this.name = name;
+        this.limit = limit;
+        this.spent = spent;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public double getLimit() {
+        return limit;
+    }
+    public double getSpent() {
+        return spent;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+    public void setSpent(double spent) {
+        this.spent = spent;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BudgetCategory [name= " + name + ", limit= " + limit + ", spent= " + spent + "]";
+    }
+
+    
+    
+    
+
+    
     
 }
